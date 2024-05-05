330af13a72cce38e74e770ff7d23856b70bea9d8
RafaNandaA <139905333+RafaNandaA@users.noreply.github.com>
2024-05-05 18:54:13 +0700

    Update ExoMod.py

--- ExoMod.py
+++ ExoMod.py
@@ -128,6 +128,89 @@ def Fitting(data_time, data_flx, visualize=True, errordisplay=False):
             plt.gca().invert_yaxis()
             plt.show()
     return x + data_time[0] + center, y + base
+def FittingErr(data_time, data_flx, data_flx_err, visualize=True, errordisplay=False):
+    '''
+    This function is the same as the previous function, but accommodate the error value for the magnitude data.
+    The input of the error value can be a numpy array or just a single value (float).
+    '''
+
+    def Rsquare(params, data_time=data_time, data_flx=data_flx, data_flx_err=data_flx_err):
+        init_ratio, init_impact, init_transit, init_center, init_base = params
+        x, y = model(init_ratio, init_impact, init_transit)
+        center = init_center
+        base = init_base
+        yfit = np.interp(data_time, x + data_time[0] + center, y + base)
+        y_mean = np.mean(data_flx)
+        SST = np.sum((data_flx - y_mean)**2)
+        SSR = np.sum(((yfit - data_flx) / data_flx_err)**2)
+        R_squared = 1 - (SSR / SST)
+        return -R_squared
+    if errordisplay==False: 
+        bounds = [(0, 1), (0, 1), (1e-8, 1), (0, (max(data_time)-min(data_time))), (min(data_flx), max(data_flx))]   
+        result = differential_evolution(Rsquare, bounds)
+        result_final = minimize(Rsquare, result.x, method='BFGS')  
+
+        best_R_squared = -result_final.fun 
+        best_params = result_final.x
+
+        print("Best R_squared:", best_R_squared)
+        print("Best parameters:", best_params)
+
+        init_ratio, init_impact, init_transit, init_center, init_base = best_params
+        x, y = model(init_ratio, init_impact, init_transit)
+        center = init_center
+        base = init_base
+        print("rasio= ", init_ratio)
+        print("impact param= ", init_impact)
+        print("transit duration= ", init_transit)
+    else:
+        def Rsquare(params, data_time=data_time, data_flx=data_flx):
+            init_ratio, init_impact, init_transit, init_center, init_base = params
+            x, y = model(init_ratio, init_impact, init_transit)
+            center = init_center
+            base = init_base
+            yfit = np.interp(data_time, x + data_time[0] + center, y + base)
+            y_mean = np.mean(data_flx)
+            SST = np.sum((data_flx - y_mean)**2)
+            SSR = np.sum((yfit - data_flx)**2)
+            R_squared = (SST - SSR) / SST
+            return -R_squared  
+        bounds = [(0, 1), (0, 1), (1e-8, 1), (0, (max(data_time)-min(data_time))), (min(data_flx), max(data_flx))]   
+        result = differential_evolution(Rsquare, bounds)
+        result_final = minimize(Rsquare, result.x, method='BFGS') 
+        best_R_squared = -result_final.fun 
+        best_params = result_final.x
+        bootstrap_params = []
+        for _ in range(100):
+            indices = np.random.choice(len(data_time), size=len(data_time), replace=True)
+            bootstrap_time = data_time[indices]
+            bootstrap_flx = data_flx[indices]
+
+            bootstrap_result = minimize(Rsquare, best_params, method='BFGS', args=(bootstrap_time, bootstrap_flx))
+            bootstrap_params.append(bootstrap_result.x)
+
+        bootstrap_params = np.array(bootstrap_params)
+        parameter_uncertainties = np.std(bootstrap_params, axis=0)
+        
+        print("Best parameters:")
+        print("    ratio =", best_params[0], "±", parameter_uncertainties[0])
+        print("    impact param =", best_params[1], "±", parameter_uncertainties[1])
+        print("    transit duration =", best_params[2], "±", parameter_uncertainties[2])
+
+        init_ratio, init_impact, init_transit, init_center, init_base = best_params
+        x, y = model(init_ratio, init_impact, init_transit)
+        center = init_center
+        base = init_base
+    if visualize==True:
+        plt.figure(figsize=(16, 5))
+        # plt.scatter(data_time, data_flx)
+        plt.errorbar(data_time, data_flx, yerr=data_flx_err, fmt='o', capsize=3,zorder=1)
+        plt.plot(x + data_time[0] + center, y + base, color='red',zorder=2)
+        plt.xlabel('time (days)')
+        plt.ylabel('normalized mag')
+        plt.gca().invert_yaxis()
+        plt.show()
+    return x + data_time[0] + center, y + base
 def Lightcurve_Stacking(timedata, fluxdata, visualize=False):
     '''
     This function is used for multiple light curves Stacking.
@@ -163,3 +246,45 @@ def residual(JD_all, mag_all, timemodel, magmodel):
     plt.legend()
     plt.gca().invert_yaxis()
     plt.show()
+def model_LD(ratio,impact,t_transit,u):
+    def num(r,u):
+        """
+        Linear surface brightness function for Limb Darkening Model 
+        """
+        return 1*(1 - u*(1-(1-r**2)**0.5))
+    def generate_points(a, b, r, num_points, u):
+        points = []
+        for i in range(num_points):
+            r_sampled = r*i/num_points
+            try:
+                density=int(num(i/num_points,u)*r_sampled)
+            except:
+                print(r_sampled)
+            theta = np.linspace(-np.pi/2, 3*np.pi/2,density)
+            print(theta)
+            for j in range(density):
+                x = a + r_sampled * np.cos(theta[j])
+                y = b + r_sampled * np.sin(theta[j])
+                points.append((x, y))
+        return points
+    points=generate_points(0,0,100,u)
+    def uncovered_area_ld(a2, b2, r2, points):
+        count = 0
+        for point in points:
+            x, y = point
+            if (x-a2)**2 + (y-b2)**2 > r2**2:
+                count += 1
+        return count
+    R1=100
+    R2=ratio*R1
+    v=2*(R1+R2)/t_transit
+    time_span=10*R1/v
+    time_sample=np.linspace(0,time_span,1000)
+    normalized_time=time_sample-np.median(time_sample)
+    a1=-5*R1
+    b1=0
+    flux_points2=[]
+    for i in range(len(time_sample)):
+        flux_points2.append(uncovered_area_ld((a1+v*time_sample[i]),b1+(impact*R1),R2,points))
+    flux_points2=-2.5*np.log10(np.array(flux_points2)/len(points))
+    return normalized_time, flux_points2
