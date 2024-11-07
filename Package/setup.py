c2e847adc03781b4b66da3c66808c8a9cc89986e
RafaNandaA <139905333+RafaNandaA@users.noreply.github.com>
2024-11-08 03:30:05 +0700

    Update setup.py

--- Package/setup.py
+++ Package/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',  # Replace with your package's name
+    name='ExoMod', 
     version='0.1',
     description='A package for light curve fitting using numerical optimization',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/my_package',  # Replace with your repository URL
+    url='https://github.com/RafaNandaA/ExoMod/edit/main/Package',
     packages=find_packages(),
     install_requires=[
         'numpy',
